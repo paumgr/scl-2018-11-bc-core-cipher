5ec6b5333afa80586678247a0f994de8c258d46a
Paumgr <PauGR@MacBook-Pro-de-Paula.local>
2018-12-07 00:26:39 -0300

    subiendo avancede de proyecto cifrado cesar

--- cipher.js
+++ cipher.js
@@ -1,8 +1,34 @@
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+   encode: (cifrar , number) => {
+    let encriptar = "";
+    for (let i=0; i<cifrar.length; i++){
+      let ascii_mensaje= cifrar.charCodeAt(i);
+      if(ascii_mensaje >= 65 && ascii_mensaje <=90){
+          ascii_mensaje = ((ascii_mensaje - 65 + number) % 26) + 65;
+      }
+      encriptar = encriptar + String.fromCharCode(ascii_mensaje);
+     }
+     return encriptar;
+
+}, 
+
+
+  decode: (decifrar , numbers) => {
+    let decifrando = "";
+    for (let i=0; i<decifrar.length; i++){
+      let ascii = decifrar.charCodeAt(i);
+      let asciiTransformado = ((ascii - 65 - numbers) % 26 + 65);
+      if (decifrar[i]== " "){
+        decifrando = decifrando + " ";
+    }else if((ascii - 65 - numbers) >= 0  ){
+        decifrando = decifrando + String.fromCharCode(asciiTransformado);
+      }else{
+        asciiTransformado = asciiTransformado + 26;
+        decifrando = decifrando + String.fromCharCode(asciiTransformado);
+      }
+    }
+    return decifrando;
+ }
+
+}
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-/* Acá va tu código */
+window.onload = () => {
+    //obtengo mi div que es contenedor que contiene mi codigo js
+    const cifrando = document.getElementById("contenedor1");
+    //le digo al boton que al hacer click realice una accion
+    document.getElementById ("botonOk").addEventListener ("click", (accionCifrar) => {
+        accionCifrar.preventDefault();
+        let cifrar = (document.getElementById ("cifrado").value).toUpperCase();
+        let number = parseInt(document.getElementById ("offset").value);
+        
+    cifrando.innerHTML = cipher.encode(cifrar , number)
+});
+ //obtengo mi div que es contenedor que contiene mi codigo js
+ const decifrando = document.getElementById("contenedor2");
+ //le digo al boton que al hacer click realice una accion
+ document.getElementById ("botonOk2").addEventListener ("click", (accionDecifrar) => {
+     accionDecifrar.preventDefault();
+     let decifrar = (document.getElementById ("decifrado").value).toUpperCase();
+     let numbers = parseInt(document.getElementById ("offset2").value);
+     
+ decifrando.innerHTML = cipher.decode(decifrar , numbers)
+});
+}
