57d44a6b911f181e9d5368a29bbbdd48da244169
Paumgr <PauGR@MacBook-Pro-de-Paula.local>
2018-12-11 08:02:42 -0300

    JS: se agrega boton limpiar, modifica cipher. HTML: agrega link cifrado cesar

--- cipher.js
+++ cipher.js
@@ -4,11 +4,27 @@ window.cipher = {
     let encriptar = "";
     for (let i=0; i<cifrar.length; i++){
       let ascii_mensaje= cifrar.charCodeAt(i);
-      if(ascii_mensaje >= 65 && ascii_mensaje <=90){
+
+      if(cifrar[i] == " "){
+        encriptar = encriptar + " ";
+      }else if(ascii_mensaje >= 65 && ascii_mensaje <=90){
           ascii_mensaje = ((ascii_mensaje - 65 + number) % 26) + 65;
-      }
+          
       encriptar = encriptar + String.fromCharCode(ascii_mensaje);
+    }
+
+     if(ascii_mensaje >= 48 && ascii_mensaje <= 57) {
+      ascii_mensaje = ((ascii_mensaje - 48 + number) % 10) + 48;
+  
+     encriptar = encriptar + String.fromCharCode(ascii_mensaje);
      }
+
+     if(ascii_mensaje >= 97 && ascii_mensaje <= 122) {
+      ascii_mensaje = ((ascii_mensaje - 97 + number) % 26) + 97;
+  
+     encriptar = encriptar + String.fromCharCode(ascii_mensaje);
+     }
+ }
      return encriptar;
 
 }, 
@@ -17,18 +33,22 @@ window.cipher = {
   decode: (decifrar , numbers) => {
     let decifrando = "";
     for (let i=0; i<decifrar.length; i++){
-      let ascii = decifrar.charCodeAt(i);
-      let asciiTransformado = ((ascii - 65 - numbers) % 26 + 65);
-      if (decifrar[i]== " "){
-        decifrando = decifrando + " ";
-    }else if((ascii - 65 - numbers) >= 0  ){
-        decifrando = decifrando + String.fromCharCode(asciiTransformado);
-      }else{
-        asciiTransformado = asciiTransformado + 26;
-        decifrando = decifrando + String.fromCharCode(asciiTransformado);
-      }
+      let ascii_mensaje = decifrar.charCodeAt(i);
+     if(decifrar[i] == " "){
+       decifrando = decifrando + " ";
+     }else if(ascii_mensaje >=65 && ascii_mensaje <=90){
+          ascii_mensaje = ((ascii_mensaje - 65 - numbers + 26) % 26)+65;
+      decifrando = decifrando + String.fromCharCode(ascii_mensaje);
     }
+    if(ascii_mensaje >=48 && ascii_mensaje <=57){
+      ascii_mensaje =((ascii_mensaje - 57 - numbers )%10) +57;
+    decifrando = decifrando + String.fromCharCode(ascii_mensaje);
+    }
+    if(ascii_mensaje >=97 && ascii_mensaje <=122){
+      ascii_mensaje =((ascii_mensaje - 97 - numbers )%26) +97;
+    decifrando = decifrando + String.fromCharCode(ascii_mensaje);
+    }
+   }
     return decifrando;
- }
-
-}
+  }
+};
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 window.onload = () => {
-   
-    const cifrando = document.getElementById("contenedor1");
-    
-    document.getElementById ("botonOk").addEventListener ("click", (accionCifrar) => {
-        accionCifrar.preventDefault();
-        let cifrar = (document.getElementById ("cifrado").value).toUpperCase();
-        let number = parseInt(document.getElementById ("offset").value);
-        
-    cifrando.innerHTML = cipher.encode(cifrar , number)
-});
- 
- const decifrando = document.getElementById("contenedor2");
 
- document.getElementById ("botonOk2").addEventListener ("click", (accionDecifrar) => {
-     accionDecifrar.preventDefault();
-     let decifrar = (document.getElementById ("decifrado").value).toUpperCase();
-     let numbers = parseInt(document.getElementById ("offset2").value);
-     
- decifrando.innerHTML = cipher.decode(decifrar , numbers)
-});
+    const cifrando = document.getElementById("contenedor1");
+
+    document.getElementById("botonOk").addEventListener("click", (accionCifrar) => {
+        accionCifrar.preventDefault();
+        let cifrar = document.getElementById("cifrado").value;
+        let number = parseInt(document.getElementById("offset").value);
+
+        cifrando.innerHTML = cipher.encode(cifrar, number)
+    });
+
+    const decifrando = document.getElementById("contenedor2");
+
+    document.getElementById("botonOk2").addEventListener("click", (accionDecifrar) => {
+        accionDecifrar.preventDefault();
+        let decifrar = document.getElementById("decifrado").value;
+        let numbers = parseInt(document.getElementById("offset2").value);
+
+        decifrando.innerHTML = cipher.decode(decifrar, numbers)
+    });
+
 }
+
+function reset() {
+    document.getElementById("cifrado").value = "";
+    document.getElementById("decifrado").value = "";
+}
+
